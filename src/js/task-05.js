f498b815a3547ddc81124a80a1e2d4f06f5a34c4
VladislavFedorchuk <142826785+VladislavFedorchuk@users.noreply.github.com>
2023-12-12 11:46:21 +0200

    finisched

---
+++ src/js/task-05.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+});
